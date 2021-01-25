19cace2a7cf02e5bbd3baa4bf3c674c7088c1331
Elias Moura <d.eliasmoura2@gmail.com>
2021-01-25 19:36:21 -0300

    master

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,13 +3,4 @@ module.exports = {
   getIndex: (req, res, next) => {
     res.render('index')
   },
-  registro: (req, res) => {
-    console.log(req.body)
-    // res.redirect('/')
-    res.send(req.body)
-  },
-  profile: (req, res) => {
-    console.log('asd')
-    res.send('asda')
-  }
 }
--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,7 +3,5 @@ const router = Router()
 const indexController = require('../controllers/index.controller');
 
 router.get('/', indexController.getIndex)
-router.post('/register', indexController.registro)
-router.get('/profile', indexController.profile)
 
 module.exports = router
